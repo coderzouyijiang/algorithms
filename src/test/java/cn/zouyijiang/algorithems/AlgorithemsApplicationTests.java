685689e5353b40bfe16b1d6794ff7d996cd63235
zouyijiang <zouyijiang@163.com>
2020-07-19 09:56:46 +0800

    算法和数据结构学习，项目初始化

---
+++ src/test/java/cn/zouyijiang/algorithems/AlgorithemsApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.zouyijiang.algorithems;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgorithemsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
